b241005df34367d05342b16800a461e927faa7c2
MBK <mohammadbaherk@gmail.com>
2023-03-14 00:19:33 +0200

    commit4

--- main.js
+++ main.js
@@ -6,11 +6,40 @@ bars.onclick = function () {
 };
 
 // <!-- Initialize Swiper -->
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
 let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 0,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  speed: 1000,
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
   },
 });
