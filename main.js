e6945e9489e0416de52020a78f4acabb4ac3f143
MBK <mohammadbaherk@gmail.com>
2023-03-13 19:50:18 +0200

    commit3

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ bars.onclick = function () {
   navbar.classList.toggle("active");
   bars.classList.toggle("open");
 };
+
+// <!-- Initialize Swiper -->
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
